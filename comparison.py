68c0af23e4f80b622c293c16464c67b1cbb81486
blairarmstrong <blair.c.armstrong@gmail.com>
2017-06-11 08:02:56 -0400

    adding updated filters

--- comparison.py
+++ comparison.py
@@ -5,13 +5,14 @@ Created on Tue May 23 13:47:13 2017
 
 @author: caitlin
 """
-     
+
 #import nltk
 import pickle
 import numpy as np
 #import scipy
 #from scipy import spatial
 import math
+import os.path
 
 def square_rooted(x):
 
@@ -22,85 +23,93 @@ def cosine_similarity(x,y):
     numerator = sum(a*b for a,b in zip(x,y))
     denominator = square_rooted(x)*square_rooted(y)
     return (numerator/float(denominator))
-    
 
 
-                        
+
+
 #Import dictionary vectors from pickled file (defsspace)
 defsspace = pickle.load(open("./eDom_filter/defsspace/defsspace.p", "rb"))
 outfile = "./output/meaningfreq.txt"
 correlationfile = "./output/correlation.txt"
-    
-#Import wikipedia .mat files, convert txt to matrix arrays 
- 
+
+#Import wikipedia .mat files, convert txt to matrix arrays
+
 fout = open(outfile, "w")
 
-ds = {}
-ds['bank'] = defsspace['bank']
-ds['ear'] = defsspace['ear']
-ds['compound'] = defsspace['compound']
-defsspace = ds;
+#ds = {}
+#ds['chow'] = defsspace['chow']
+#ds['ear'] = defsspace['ear']
+#ds['compound'] = defsspace['compound']
+#defsspace = ds;
 
 bigdict = {}
 
+sofar = 0;
 for k in defsspace:
-    
-    f=open('./critwindows/'+k+'.mat','r')
-    def_arr = np.genfromtxt('./critwindows/'+k+'.mat')
+    print(k);
+    print(sofar)
+    sofar = sofar+1;
+    if os.path.isfile('./critwindows/'+k+'.mat'):
+        f=open('./critwindows/'+k+'.mat','r')
+        def_arr = np.genfromtxt('./critwindows/'+k+'.mat')
 
-    arr = np.zeros([len(defsspace[k]), len(def_arr)])
+        arr = np.zeros([len(defsspace[k]), len(def_arr)])
 
-    for m in defsspace[k]:
-        print(defsspace[k][m][0])
-        for i in range (0,len(def_arr)):
-            #print(i)
-            cossim = cosine_similarity(defsspace[k][m][0], def_arr[i])
-            arr[int(m)-1, i] = cossim
-                
-    #find out which meaning won on each row.
-    meaningcount = np.zeros([len(defsspace[k]), 1])
-    
-    
-    for j in range (0, len(def_arr)):
-        #find over m rows which is largest
-        bigcos = -2.0
-        winner = -1;
-        #n=-1
-        for n in range(0,len(defsspace[k])):
-            curVal = arr[n, j]
-            if  curVal > bigcos:
-                bigcos = curVal;
-                winner = n;
+        for m in defsspace[k]:
+            for i in range (0,len(def_arr)):
+#                print("full critwindow file"+str(def_arr))
+#                print("Details of defspace")
+#                print(k)
+#                print(defsspace[k][m][0])
+#                print("Defails of def arr")
+#                print(def_arr[i])
+                cossim = cosine_similarity(defsspace[k][m][0], def_arr[i])
+                arr[int(m)-1, i] = cossim
 
-        meaningcount[winner] = meaningcount[winner]+1
-        
-    tot = sum(meaningcount)
-    
-    for i in range(len(meaningcount)):
-        meaningcount[i]=meaningcount[i]/tot*100;
-    
-    meaningcount = meaningcount.round();
-                                     
-    print(meaningcount)
-    
-    biggest = max(meaningcount)
-    
-    bigdict[k] = biggest;
-    
-    fout.write(k +"\t"+ str(round(biggest[0])) + "\t") 
-    
-    for i in range(len(meaningcount)):
-         fout.write(str(round(meaningcount[i][0]))+"\t")
-        
-    fout.write(str(tot[0]))
-    fout.write("\n")
-    
-    
-    
-    
+        #find out which meaning won on each row.
+        meaningcount = np.zeros([len(defsspace[k]), 1])
+
+
+        for j in range (0, len(def_arr)):
+            #find over m rows which is largest
+            bigcos = -2.0
+            winner = -1;
+            #n=-1
+            for n in range(0,len(defsspace[k])):
+                curVal = arr[n, j]
+                if  curVal > bigcos:
+                    bigcos = curVal;
+                    winner = n;
+
+            meaningcount[winner] = meaningcount[winner]+1
+
+        tot = sum(meaningcount)
+
+        for i in range(len(meaningcount)):
+            meaningcount[i]=meaningcount[i]/tot*100;
+
+        meaningcount = meaningcount.round();
+
+        #print(meaningcount)
+
+        biggest = max(meaningcount)
+
+        bigdict[k] = biggest;
+
+        fout.write(k +"\t"+ str(round(biggest[0])) + "\t")
+
+        for i in range(len(meaningcount)):
+             fout.write(str(round(meaningcount[i][0]))+"\t")
+
+        fout.write(str(tot[0]))
+        fout.write("\n")
+
+
+
+
 fout.close()
 
-    
+
 #compute correlation for all words that we have found so far
 
 #load in eDom biggest file
@@ -111,18 +120,35 @@ eDom = {}
 
 for line in open('./input/eDom_biggest.txt'):
     line = line.split()
+    print(line)
     eDom[line[0]] = int(line[1])
-    
+
 edomUnion = {};
 
+print("Printing bigdict")
+print(bigdict)
+
 for k in bigdict:
-    edomUnion[k] = eDom[k]
-    
-allbiggest = np.zeros([2, len(edomUnion)])
+    if k in eDom:
+        #Safety code because eDom does not contain every word in bigdict?
+        edomUnion[k] = eDom[k]
+    else:
+        print("ERROR: Missing following item in edom: "+k)
+
+print("Number of observations in bigdict")
+print(len(bigdict))
+
+print("Number of observations in edomUnion")
+print(len(bigdict))
+###---> below used to be edomUnion instead of bigdict.
+allbiggest = np.zeros([2, len(bigdict)])
 i=0;
 for k in bigdict:
-    allbiggest[0][i] = bigdict[k]
-    allbiggest[1][i] = edomUnion[k]
+    if k in edomUnion:
+        allbiggest[0][i] = bigdict[k]
+        allbiggest[1][i] = edomUnion[k]
+    else:
+        print("ERROR: Missing following item in edomUnion: "+k)
     i=i+1
 
 #https://stackoverflow.com/questions/26714048/numpy-arrays-correlation
@@ -149,20 +175,22 @@ def corr_pearson(x, y):
 
     return corr
 
-    
+
 print(corr_pearson(allbiggest[0], allbiggest[1]))
 print('Correlation between eDom and our method:')
-print(np.corrcoef(allbiggest[0], allbiggest[1])[0][1].round(decimals=3))
+print(corr_pearson(allbiggest[0], allbiggest[1]))
+#print(np.corrcoef(allbiggest[0], allbiggest[1])[0][1].round(decimals=3))
+
 
 fc = open(correlationfile, "w")
-fc.write(str(np.corrcoef(allbiggest[0], allbiggest[1])[0][1].round(decimals=3)))
+fc.write(str(np.corrcoef(allbiggest[0], allbiggest[1])))
 fc.close()
 
 
 
 
-    
-    
+
+
 
 
 
--- wikitocritwindow.py
+++ wikitocritwindow.py
@@ -8,7 +8,7 @@ from os.path import isfile, join
 from nltk import word_tokenize, wordpunct_tokenize
 import codecs
 import nltk
-import dill as pickle #need advanced pickling to pickle nltk lamba 
+import dill as pickle #need advanced pickling to pickle nltk lamba
 import pathos.multiprocessing as multiprocessing
 import numpy as np
 
@@ -34,9 +34,9 @@ sendictfile = 'sendict.p'
 
 semdim = 300
 
-nProcs = 5;
+nProcs = 30;
 
-###############################################################################   
+###############################################################################
 
 class ConcordanceIndex2(ConcordanceIndex):
     def create_concordance(self, word, token_width=11):
@@ -53,57 +53,57 @@ class ConcordanceIndex2(ConcordanceIndex):
 
 def createConcordances(fname):
     print("Processing file: "+fname)
-    
+
     f = codecs.open(wikidir+fname,"r","utf-8")
     print(fname)
     raw = f.read();
     print(fname)
     tokens = wordpunct_tokenize(raw);
     text = nltk.Text(tokens)
-    
-    lenRawText = len(text) # number of words in the cleaned dsocument.  
+
+    lenRawText = len(text) # number of words in the cleaned dsocument.
     print("Current cleaned article length: " + str(lenRawText))
-    
+
     print(text)
-    
+
     #load list of words from frequency filtered list
     f2 = open(mostfreqfile,"r")
-    
+
     mostfreq = [];
     with open(mostfreqfile) as f2:
         for line in f2:
-            mostfreq.append(line.strip())  
+            mostfreq.append(line.strip())
     ##print("number of words in frequency file")
     #print(len(mostfreq))
     #print(mostfreq)
 
-    
+
     #clean up the processed text as desired
     #1a.  Convert text to lowercase, remove spaces around words
     #1b.  Removing "words" that are not alphabetical.
     #2.  Remove words that are 1 character in length
     #3.  Not in the stopword list for nltk English
     #4   Only keep most frequent words as specified in external file
-    words = [w.lower().strip() for w in text if w.isalpha() and 
-              len(w) > 1 and 
+    words = [w.lower().strip() for w in text if w.isalpha() and
+              len(w) > 1 and
               w not in set(nltk.corpus.stopwords.words('english')) and
               w in mostfreq]
-    
+
     #create a new text object based on the "cleaned text"
     raw2 = " ".join(words)
     tokens2 = word_tokenize(raw2)
     text2 = nltk.Text(tokens2)
-    
-    lenCleanText = len(text2) # number of words in the cleaned dsocument.  
+
+    lenCleanText = len(text2) # number of words in the cleaned dsocument.
     print("Current cleaned article length: " + str(lenCleanText))
-    
+
     print("Percent of Tokens removed during cleaning")
     print((lenRawText-lenCleanText)/lenRawText*100)
 
-    #create concordance array based on cleaned text  , pickle resulting object  
+    #create concordance array based on cleaned text  , pickle resulting object
     print("Calculating Concordances")
     c = ConcordanceIndex2(tokens2)
-    pickle.dump(c,open(concorddir+fname, 'wb'))                   
+    pickle.dump(c,open(concorddir+fname, 'wb'))
     print("Completed Calculating Concordances")
 
     #Calculate and save frequency information for the text
@@ -121,7 +121,7 @@ def critWindows(t):
     sendict = pickle.load(open(sendictfile, "rb"))
     fl = [f for f in listdir(concorddir) if isfile(join(concorddir, f))]
     i = 0;
-    
+
     for fname in fl:
         print("t: " + t + " f: " + fname)
         #reload in the pickeled concordance file from prev step:
@@ -131,67 +131,68 @@ def critWindows(t):
         l = c.create_concordance(t)
         #c.print_concordance(h)
         #print(l) #lists the concordance in memory
-        
+
         f2 = open(critwindowdir+t+".txt","a")
         f3 = open(critwindowdir+t+".mat","a")
         j=0;
         newvec = np.zeros([1, semdim]);
-        
+
         for e in l:
             f2.write(' '.join(e) + "\r\n")
             j=j+1;
-        
+
             for g in e:
                 if g != t:
-                    newvec = newvec+sendict[g]
-            
+                    if g in sendict:
+                        newvec = newvec+sendict[g]
+
             print('\r\n')
             print(newvec)
             print(len(newvec[0]))
             #print(newvec.shape())
             print('\r\n')
-            
-            
+
+
             newlist=newvec[0].tolist();
             print('\r\n')
-            
+
             print(newlist)
             print('\r\n')
             print(len(newlist))
-            
+
             newstr = ' '.join((f'{h:.6f}') for h in newlist)
             f3.write(newstr)
             f3.write('\r\n')
-            
-        
+
+
         f2.close();
         f3.close();
         i = i+j;
-    
+
     return(t + " occured " + str(i) + " times")
 
 
 ###############################################################################
 #main loop to be executed by master processor
-if __name__=='__main__':     
-    
+if __name__=='__main__':
+
     print("\r\n\r\n")
-    
+
     #clear critical window directory
     torm = os.listdir(concorddir)
     for f in torm:
         if f.endswith(".txt"):
             os.remove(join(concorddir,f))
-            
+
     #clear frequency directory
     torm = os.listdir(freqdir)
     for f in torm:
         if f.endswith(".freq"):
             os.remove(join(freqdir,f))
-    
-    
+
+
     fl = [f for f in listdir(wikidir) if isfile(join(wikidir, f))]
-    
+
     print("List of files to process")
     print(fl)
     print("\r\n")
@@ -201,14 +202,14 @@ if __name__=='__main__':
     pool= multiprocessing.Pool(processes=nProcs)
     print("pool initialized")
     print("\r\n")
-    
+
     results = [pool.apply_async(createConcordances,args=(x,)) for x in fl]
     print("Waiting to display results of async processing")
     output = [p.get() for p in results]
-    
+
     print("Total number of words after cleaning, across all files:")
     print(sum(output))
-    
+
     print("\r\n")
     print("task complete --- total time:")
     print(datetime.now()-startTime);
@@ -223,7 +224,7 @@ if __name__=='__main__':
             os.remove(join(critwindowdir,f))
         if f.endswith(".mat"):
             os.remove(join(critwindowdir,f))
-    
+
     #load list of words to get critical windows for
     targetList = open(targFile).read().splitlines();
 
@@ -231,14 +232,14 @@ if __name__=='__main__':
     pool= multiprocessing.Pool(processes=5)
     print("pool initialized")
     print("\r\n")
-        
+
     results = [pool.apply_async(critWindows,args=(t,)) for t in targetList]
     print("Waiting to display results of async processing")
     output = [p.get() for p in results]
     print("Number of critical windows for each word")
     for e in output:
         print("\t"+e)
-    
+
 
     print("\r\n")
     print("task complete --- total time:")
